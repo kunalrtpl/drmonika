332d89d9cb3ef45dda7ca40707a714b2412e1b75
kunal_royalways <kunal@rtpltech.com>
2025-08-11 16:13:07 +0530

    Intial commit

--- config/siteConfig.js
+++ config/siteConfig.js
@@ -1,10 +1,10 @@
 // config/siteConfig.js
 export const siteConfig = {
-  appName: process.env.NEXT_PUBLIC_APP_NAME,
+  appName: process.env.NEXT_PUBLIC_APP_NAME || "",
   description: "",
-  contactNumber: process.env.NEXT_PUBLIC_CONTACT_NUMBER,
-  email: process.env.NEXT_PUBLIC_EMAIL,
-  facebook: process.env.NEXT_PUBLIC_FACEBOOK_LINK,
+  contactNumber: process.env.NEXT_PUBLIC_CONTACT_NUMBER || "",
+  email: process.env.NEXT_PUBLIC_EMAIL || "",
+  facebook: process.env.NEXT_PUBLIC_FACEBOOK_LINK || "",
   // Add navLinks here for header & footer navigation
   navLinks: [
     { name: "Home", href: "/" },
